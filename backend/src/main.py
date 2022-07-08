f57f9eb9ce4ff2fcdcee844c435896b0754cd362
Cássio Fernando <61878987+cassiofb-dev@users.noreply.github.com>
2022-07-07 21:12:37 -0300

    Revert "fastapi: data processing - saving progress..."

--- backend/src/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-from src.core.orjson import ORJSONResponse
-from src.alerta_rio.alerta_rio_controller import alerta_rio_controller
-
-app = FastAPI(default_response_class=ORJSONResponse)
-
-app.include_router(alerta_rio_controller)
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule }  from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
 import { DocsComponent } from './routes/docs/docs.component';
 import { AboutComponent } from './routes/about/about.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeService } from './routes/home/home.service';
 
 @NgModule({
   declarations: [
@@ -22,10 +21,6 @@ import { HomeService } from './routes/home/home.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
-  providers:[
-    HomeService
   ],
   bootstrap: [AppComponent],
 })
